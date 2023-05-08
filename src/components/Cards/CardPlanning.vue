129ae061b47dcc90d7cc6fdfc94443755e2e0cc9
sirine <sirinebalghouthi59@gmail.com>
2023-05-08 23:07:22 +0200

    feat : planning

--- src/components/Cards/CardPlanning.vue
+++ src/components/Cards/CardPlanning.vue
@@ -1,134 +1,174 @@
 <template>
-    <div class="calendar">
-      <div class="header">
-        <button @click="previousMonth">&lt;</button>
-        <h2>{{ month }} {{ year }}</h2>
-        <button @click="nextMonth">&gt;</button>
+  <div
+    class="relative flex min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded"
+    style="border-radius: 1rem; max-height: 31.7rem"
+  >
+    <div class="wrapper">
+      <header>
+        <p class="current-date"></p>
+        <div class="icons">
+          <span id="prev" class="material-symbols-outlined">chevron_left</span>
+          <span id="next" class="material-symbols-outlined">chevron_right</span>
+        </div>
+      </header>
+      <div class="calendar">
+        <ul class="weeks">
+          <li>Sun</li>
+          <li>Mon</li>
+          <li>Tue</li>
+          <li>Wed</li>
+          <li>Thu</li>
+          <li>Fri</li>
+          <li>Sat</li>
+        </ul>
+        <ul class="days"></ul>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th v-for="day in days" v-bind:key="day">{{ day }}</th>
-          </tr>
-        </thead>
+    </div>
+    <div style="width: 22rem;max-height: 27rem;margin: 1rem;border-radius: 1rem;padding-top: 1.5rem;padding-bottom: 1.5rem;">
+      <!-- Projects table -->
+      <table class="items-center w-full">
+       
         <tbody>
-          <tr v-for="week in weeks" v-bind:key="week">
-            <td v-for="day in week" v-bind:key="day.date" :class="getClass(day)" @click="selectDate(day)">
-              <slot name="date" v-bind:date="day"></slot>
-              {{ day.date }}
+          <tr>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-500 text-xss whitespace-nowrap text-center"
+            >
+              12:30
+            </td>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-700 text-xss text-center p-4"
+            >
+              match
             </td>
           </tr>
+          <tr>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-500 text-xss whitespace-nowrap text-center"
+            >
+              12:30
+            </td>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-700 text-xss text-center p-4"
+            >
+              match
+            </td>
+          </tr>
+          <tr>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-500 text-xss whitespace-nowrap text-center"
+            >
+              12:30
+            </td>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-700 text-xss text-center p-4"
+            >
+              match
+            </td>
+          </tr>
+          <tr>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-500 text-xss whitespace-nowrap text-center"
+            >
+              12:30
+            </td>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-700 text-xss text-center p-4"
+            >
+              match
+            </td>
+          </tr>
+          <tr>
+            <td style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-500 text-xss whitespace-nowrap text-center"
+            >
+              12:30
+            </td>
+            <td  style="border:1px solid #fff;"
+              class="px-6 align-middle py-3 font-semibold text-blueGray-700 text-xss text-center p-4"
+            >
+              match
+            </td>
+          </tr>
+        
         </tbody>
       </table>
     </div>
-  </template>
-  <script>
-  export default {
-    data() {
-      return {
-        today: new Date(),
-        selectedDate: null,
-        currentYear: null,
-        currentMonth: null,
-        days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-        weeks: [],
-      };
-    },
-    computed: {
-      year() {
-        return this.currentYear;
-      },
-      month() {
-        const months = [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        ];
-        return months[this.currentMonth];
-      },
-    },
-    methods: {
-      generateCalendar() {
-        const year = this.currentYear;
-        const month = this.currentMonth;
-        const firstDay = new Date(year, month, 1).getDay();
-        const daysInMonth = new Date(year, month + 1, 0).getDate();
-        let date = 1;
-        this.weeks = [];
-        for (let i = 0; i < 6; i++) {
-          let week = [];
-          if (i === 0) {
-            for (let j = 0; j < firstDay; j++) {
-              week.push({ date: "" });
-            }
-            for (let j = firstDay; j < 7; j++) {
-              week.push({ date: date, isCurrentMonth: true });
-              date++;
-            }
-          } else {
-            for (let j = 0; j <7; j++) {
-if (date > daysInMonth) {
-week.push({ date: "" });
-} else {
-week.push({ date: date, isCurrentMonth: true });
-date++;
-}
-}
-}
-this.weeks.push(week);
-}
-},
-previousMonth() {
-if (this.currentMonth === 0) {
-this.currentYear--;
-this.currentMonth = 11;
-} else {
-this.currentMonth--;
-}
-this.generateCalendar();
-},
-nextMonth() {
-if (this.currentMonth === 11) {
-this.currentYear++;
-this.currentMonth = 0;
-} else {
-this.currentMonth++;
-}
-this.generateCalendar();
-},
-selectDate(day) {
-if (day.date !== "") {
-this.selectedDate = new Date(this.currentYear, this.currentMonth, day.date);
-this.$emit("date-selected", this.selectedDate);
-}
-},
-getClass(day) {
-let classes = [];
-if (!day.isCurrentMonth) {
-classes.push("not-current-month");
-}
-if (day.date === this.today.getDate() && this.currentYear === this.today.getFullYear() && this.currentMonth === this.today.getMonth()) {
-classes.push("today");
-}
-if (this.selectedDate && day.date === this.selectedDate.getDate() && this.currentYear === this.selectedDate.getFullYear() && this.currentMonth === this.selectedDate.getMonth()) {
-classes.push("selected");
-}
-return classes.join(" ");
-},
-},
-mounted() {
-this.currentYear = this.today.getFullYear();
-this.currentMonth = this.today.getMonth();
-this.generateCalendar();
-},
+  </div>
+
+   
+</template>
+<script>
+export default {
+  mounted() {
+    const currentDate = document.querySelector(".current-date");
+    const daysTag = document.querySelector(".days");
+    const prevNextIcon = document.querySelectorAll(".icons span");
+    let date = new Date();
+    let currYear = date.getFullYear();
+    let currMonth = date.getMonth();
+
+    const months = [
+      "Janvier",
+      "février",
+      "mars",
+      "avril",
+      "mai",
+      "juin",
+      "juillet",
+      "août",
+      "septembre",
+      "octobre",
+      "novembre",
+      "décembre",
+    ];
+
+    const renderCalendar = () => {
+      let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+        lastDayofMonth = new Date(
+          currYear,
+          currMonth,
+          lastDateofMonth
+        ).getDay(),
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+      let liTag = "";
+
+      for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+      }
+
+      for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday =
+          i === date.getDate() &&
+          currMonth === new Date().getMonth() &&
+          currYear === new Date().getFullYear()
+            ? "active"
+            : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+      }
+
+      for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+      }
+
+      currentDate.innerText = `${months[currMonth]} ${currYear}`;
+      daysTag.innerHTML = liTag;
+    };
+    renderCalendar();
+
+    prevNextIcon.forEach((icon) => {
+      icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        if (currMonth < 0 || currMonth > 11) {
+          date = new Date(currYear, currMonth);
+          currYear = date.getFullYear();
+          currMonth = date.getMonth();
+        } else {
+          date = new Date();
+        }
+        renderCalendar();
+      });
+    });
+  },
 };
 </script>
